42f36f7a481071347a387bbc8f4e98ef0d10380c
g89614544540-ship-it <g89614544540@gmail.com>
2026-03-19 23:06:02 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import uuid
 import base64
 import os
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__, static_folder='../frontend')
 
 WALLET = "UQBBklp5lYFEgYig5200TPsLjtDOnAUUGToyiFhzI6D0tP8d"
 SB_URL = "https://kjschhxyiobwlrpeoqwp.supabase.co"
